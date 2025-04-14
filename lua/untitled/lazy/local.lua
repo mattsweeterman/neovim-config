ebebc47c09d37041fcef7ffbd62f1d461e7dc123
Matthew Sweeterman <25017409+mattsweeterman@users.noreply.github.com>
2025-04-13 21:55:23 -0400

    Update local.lua

--- lua/untitled/lazy/local.lua
+++ lua/untitled/lazy/local.lua
@@ -1,13 +1,23 @@
+local home = os.getenv("HOME") or os.getenv("USERPROFILE")
+
+-- Determine platform-specific plugin path
+local plugin_dir
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  plugin_dir = home .. "\\AppData\\Local\\nvim\\pack\\themes\\start\\dracula_pro"
+else
+  plugin_dir = home .. "/.config/nvim/pack/themes/start/dracula_pro"
+end
+
 local local_plugins = {
-	{
-		"dracula_pro",
-		dir = "/Users/matthew.sweeterman/.config/nvim/pack/themes/start/dracula_pro",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme dracula_pro_van_helsing")
-		end,
-	},
+  {
+    "dracula_pro",
+    dir = plugin_dir,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme dracula_pro_van_helsing")
+    end,
+  },
 }
 
 return local_plugins
